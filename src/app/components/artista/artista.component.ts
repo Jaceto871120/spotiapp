fce8dedf56ce250a58c84d369ee567668038c05b
Jair Celis <jair.c@montechelo.com.co>
2021-02-21 13:38:39 -0500

    Implementacion de la pagina de artista

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-artista',
@@ -8,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistaComponent implements OnInit {
 
-  constructor() { }
+  artista: any = {};
+  loading;
+
+  constructor(private router: ActivatedRoute,
+              private spotityService: SpotifyService) {
+
+                this.loading = true;
+                this.router.params.subscribe( params => {
+                  this.getArtista(params.id);
+                });
+   }
 
   ngOnInit(): void {
+  }
+
+  getArtista(id: string) {
+
+    this.loading = true;
+
+    this.spotityService.getArtista(id).subscribe( artista => {
+      console.log(artista);
+      this.artista = artista;
+      this.loading = false;
+    });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -14,7 +14,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQAXThCgp1H8uRVQ_lAWo-b7FYuMqu14gDkeIfxeAZdcGRlMcl_-bsGuDJghztUHWpouukezQbbLlUi1uSRNybnDDFvbQecZ3w30S3Wef8tAbS8UVUJfZA1RvjmaqCvK8utrN2iSxKBiJFoT8zKW-WCOpOcRU4o"'
+      Authorization: 'Bearer BQCXbzOofJxZUA5_OOfVBa04ASjpAd3l2SO2NgGjByu8o9EeJPpB1-FPkdjjsqHjQfiW3s1fH86k1vMnScRwI4CH0cBgYZsHr-2IbzAPieytC5_WIl7lNi9YCrNkJCsrToiVIK8Zgx3i8kSVlUGIM2RrIj6V8rc"'
     });
 
     return this.http.get(url, {headers});
@@ -26,9 +26,14 @@ export class SpotifyService {
                 .pipe(map( (data: any) => data.albums.items));
   }
 
-  getArtista(termino: string) {
+  getArtistas(termino: string) {
 
     return this.getQuery(`search?q=${termino}&type=artist`)
                 .pipe(map((data: any) => data.artists.items));
+  }
+  getArtista(id: string) {
+
+    return this.getQuery(`artists/${id}`);
+                // .pipe(map((data: any) => data.artists.items));
   }
 }
